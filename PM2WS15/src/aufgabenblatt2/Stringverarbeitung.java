a904a2cdb85c69e3cc9db94eb4fde4cefadc0f29
Helena Lajevardi <helena.lajevardi@haw-hamburg.de>
2015-11-15 15:36:42 +0100

    Neue Klasse Stringverarbeitung erstellt.

---
+++ PM2WS15/src/aufgabenblatt2/Stringverarbeitung.java
@@ -0,0 +1,14 @@
+package aufgabenblatt2;
+
+import java.util.List;
+
+public class Stringverarbeitung {
+	
+	public static List<String> verarbeite(List<String> strList){
+		
+		
+		return strList;
+		
+	}
+
+}
