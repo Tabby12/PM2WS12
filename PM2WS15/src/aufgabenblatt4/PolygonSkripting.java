c9d32543ae9ec9e58f6d1695c49c3e205c76dd7f
Helena Lajevardi <helena.lajevardi@haw-hamburg.de>
2016-01-10 14:24:01 +0100

    Klasse PolygonSkripting kommentiert und aufgeräumt.

--- PM2WS15/src/aufgabenblatt4/PolygonSkripting.java
+++ PM2WS15/src/aufgabenblatt4/PolygonSkripting.java
@@ -1,27 +1,59 @@
+/**
+ * BTI1-PTP/03, WS 15
+ * Gruppe: Helena Lajevardi (helena.lajevardi@haw-hamburg.de)
+ *         Lennart Hartmann (lennart.hartmann@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt 4
+ * 
+ */
 package aufgabenblatt4;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Klasse zur Verarbeitung von Texteingaben.
+ * 
+ * @author Helena Lajevardi, Lennart Hartmann
+ */
 public class PolygonSkripting {
 	
-		private final Pattern pattern;
-		private Matcher matcher;
-
-		/**
-		 * Konstruktor
-		 */
-		public PolygonSkripting() {
-			pattern = Pattern.compile("(bewege)\\s->" + "\\s(0|-?[0-9]*\\.?[0-9]*),\\s?(0|-?[0-9]*\\.?[0-9]*)");
-			matcher = null;
-		}
+	/**
+	 * Muster mit dem der Befehl verglichen werden soll.
+	 */
+	private final Pattern pattern;
 	
+	/**
+	 * Objekt, dass den Befehl mit dem Muster vergleicht.
+	 */
+	private Matcher matcher;
+
+	/**
+	 * Konstruktor
+	 */
+	public PolygonSkripting() {
+		pattern = Pattern.compile("(bewege)\\s->" + "\\s(0|-?[0-9]*\\.?[0-9]*),\\s?(0|-?[0-9]*\\.?[0-9]*)");
+		matcher = null;
+	}
+	
+	/**
+	 * Ueberprueft ob der uebergebene Befehl gueltig ist.
+	 * 
+	 * @param befehl Der eingegebene Befehl.
+	 * @return gueltig, oder nicht.
+	 */
 	public boolean istGueltigerBefehl(String befehl){
 		matcher = pattern.matcher(befehl);
 		return matcher.matches();
 		
 	}
 	
+	/**
+	 * Methode, um aus dem Eingabe String die X-Koordinate zu extrahieren.
+	 * 
+	 * @param befehl Der Eingabe-String.
+	 * @return X-Koordinate aus dem Eingabe-String.
+	 * @throws Exception Bei falschen Befehl wird eine Exception geworfen.
+	 */
 	public double getX(String befehl) throws Exception {
 		if (istGueltigerBefehl(befehl)) {
 			return new Double(matcher.group(3));
@@ -30,6 +62,13 @@ public class PolygonSkripting {
 		}
 	}
 	
+	/**
+	 * Methode, um aus dem Eingabe String die Y-Koordinate zu extrahieren.
+	 * 
+	 * @param befehl Eingabe-String.
+	 * @return Y-Koordinate aus dem Eingabe-String.
+	 * @throws Exception Bei falschen Befehl wird eine Exception geworfen.
+	 */
 	public double getY(String befehl) throws Exception {
 		if (istGueltigerBefehl(befehl)) {
 			return new Double(matcher.group(2));
