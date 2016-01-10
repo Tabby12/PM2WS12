1b8835bf332fbbcdf4c68ef1ce0aa7aba38f393c
Helena Lajevardi <helena.lajevardi@haw-hamburg.de>
2016-01-10 12:50:50 +0100

    Klasse Polygon kommentiert und aufgeräumt.

--- PM2WS15/src/aufgabenblatt4/Polygon.java
+++ PM2WS15/src/aufgabenblatt4/Polygon.java
@@ -1,6 +1,7 @@
 /**
  * BTI1-PTP/03, WS 15
  * Gruppe: Helena Lajevardi (helena.lajevardi@haw-hamburg.de)
+ *         Lennart Hartmann (lennart.hartmann@haw-hamburg.de)
  * Aufgabe: Aufgabenblatt 4
  * 
  */
@@ -15,7 +16,7 @@ import com.sun.javafx.scene.paint.GradientUtils.Point;
 /**
  * Repraesentiert ein Polygon.
  * 
- * @author Helena Lajevardi
+ * @author Helena Lajevardi, Lennart Hartmann
  */
 public class Polygon extends Observable {
 	
@@ -24,33 +25,54 @@ public class Polygon extends Observable {
 	 */
 	private List<Punkt> punkte;
 	
+	/**
+	 * Die Bezeichnung fuer das Polygon.
+	 */
 	private String name;
 	
+	/**
+	 * Konstruktor.
+	 */
 	public Polygon() {
 		this.punkte = new ArrayList<Punkt>();
 	}
 	
+	/**
+	 * Fuegt dem Polygon einen neuen Punkt hinzu.
+	 * 
+	 * @param punkt der neue Punkt der hinzugefuegt werden soll.
+	 */
 	public void punktHinzufuegen(Punkt punkt){
 		punkte.add(punkt);
 		setChanged();
 		notifyObservers();
 	}
 	
+	/**
+	 * Verschiebt einen Punkt im Polygon um die angegebenen Koordinaten.
+	 * 
+	 * @param punkt der Punkt der verschoben werden soll.
+	 * @param x die X-Koordinate, um die der Punkt verschoben werden soll.
+	 * @param y die Y-Koordinate, um die der Punkt verschoben werden soll.
+	 */
 	public void punktVerschieben(Punkt punkt,int x, int y){
+		//todo: um fehlende Anweisungen ergänzen
 		setChanged();
 		notifyObservers();
-		
-		
 	}
 
+	/**
+	 * Getter.
+	 */
+	public List<Punkt> getPunkte() {
+		return punkte;
+	}
+	
 	@Override
 	public String toString() {
 		return name = "Polygon mit " + punkte.size() + " Punkten";
 	}
 
-	public List<Punkt> getPunkte() {
-		return punkte;
-	}
 	
 	
 	
