da81ae5c14f8f479b9638cbfe1d26540abb92fba
Helena Lajevardi <helena.lajevardi@haw-hamburg.de>
2016-01-10 12:56:25 +0100

    Klasse PolygonModell kommentiert und aufgeräumt.

--- PM2WS15/src/aufgabenblatt4/PolygonModell.java
+++ PM2WS15/src/aufgabenblatt4/PolygonModell.java
@@ -1,23 +1,21 @@
 /**
  * BTI1-PTP/03, WS 15
  * Gruppe: Helena Lajevardi (helena.lajevardi@haw-hamburg.de)
+ *         Lennart Hartmann (lennart.hartmann@haw-hamburg.de)
  * Aufgabe: Aufgabenblatt 4
  * 
  */
 package aufgabenblatt4;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
  * Repraesentiert ein Polygonmodell zur Verwaltung von Polygonen.
  * 
- * @author Helena Lajevardi
+ * @author Helena Lajevardi, Lennart Hartmann
  */
 public class PolygonModell extends Observable implements Observer{
 	
@@ -31,14 +29,18 @@ public class PolygonModell extends Observable implements Observer{
 	 */
 	private ObservableList<Polygon> polygonListe;
 	
+	/**
+	 * Konstruktor.
+	 */
 	public PolygonModell() {
 		polygonListe = FXCollections.observableArrayList();
 		aktuellesPolygon = new Polygon();
 		aktuellesPolygon.addObserver(this);
-		
 	}
 	
-	
+	/**
+	 * Schliesst die Bearbeitung des aktuellen Polygons ab.
+	 */
 	public void polygonBearbeitungAbschliessen(){
 		this.polygonListe.add(this.aktuellesPolygon);
 		this.aktuellesPolygon = new Polygon();
@@ -47,22 +49,20 @@ public class PolygonModell extends Observable implements Observer{
 		notifyObservers();
 	}
 	
-
-
+	/**
+	 * Getter & Setter.
+	 */
 	public ObservableList<Polygon> getPolygonListe() {
 		return polygonListe;
 	}
-
 
 	public Polygon getAktuellesPolygon() {
 		return aktuellesPolygon;
 	}
 
-
 	public void setAktuellesPolygon(Polygon aktuellesPolygon) {
 		this.aktuellesPolygon = aktuellesPolygon;
 	}
-
 
 	@Override
 	public void update(Observable o, Object arg) {
